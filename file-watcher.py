85f9616e9c441dd58b6ee8585b3534b5f2ad6d0a
go0ty <patrick.doody@rgi-corp.com>
2016-03-15 10:47:29 -0400

    Reading bucket files, creating JSON payload

--- file-watcher.py
+++ file-watcher.py
@@ -1,6 +1,7 @@
 import argparse
 import os, sys
 import boto3
+import atexit
 
 class FileWatcher:
 
@@ -14,29 +15,83 @@ class FileWatcher:
 		# Create S3 Client
 		self.client = boto3.client('s3', aws_access_key_id=self.accessKey, aws_secret_access_key=self.privateKey)
 
+		# Ensure the Persistence file exists. Create if not.
+		self.persistenceFile = open('IngestedFiles.txt', 'a+')
+
 	def listen(self):
 		"""Polls for new files in the S3 bucket."""
-		pass
+		self.scanNewFiles()
 
-	def checkIsNew(self, fileName):
+	def scanNewFiles(self):
+		"""Checks if the bucket contains any new files."""
+		# Loop through every object in the bucket and ingest new files.
+		for item in self.client.list_objects(Bucket=self.bucket).get('Contents'):
+			if self.isNewFile(item.get('Key')):
+				self.ingest(item.get('Key'))
+
+	def isNewFile(self, fileName):
 		"""Checks persistence to determine if the file has been previously ingested or not."""
-		pass
+		self.persistenceFile.seek(0)
+		return not fileName in self.persistenceFile.read()
 
 	def ingest(self, fileName):
-		"""Sends an Ingest Job to Piazza Gateway."""
-		pass
+		"""Sends an Ingest Job to Piazza Gateway for the S3 object."""
+		# Determine the Data Type, required for the JSON Payload
+		dataType = self.determineDataType(fileName)
+		if dataType is None:
+			return
 
-	def getIngestPayload(self, fileName):
+		# Get the JSON Payload for this File
+		payload = self.getIngestPayload(fileName, dataType)
+
+		# Send the Request
+
+
+	def getIngestPayload(self, fileName, dataType):
 		"""Gets the JSON Payload for the Gateway /job request."""
-		pass
+		return """
+			{
+				"apiKey": "{}",
+				"jobType": {
+					"type": "ingest",
+					"host": "{}",
+					"data": {
+						"dataType": {
+							"type": "{}",
+							"location": {
+								"type": "s3",
+								"bucketName": "{}",
+								"fileName": "elevation.tif",
+								"domainName": "s3.amazonaws.com"
+							}
+						},
+						"metadata": {
+							"description": "Ingested automatically by FileWatcher.",
+							"classType": {
+								"classification": "unclassified"
+							}
+						}
+					}
+				}
+			}
+		""".format(self.pzApiKey, False, dataType, self.bucket)
 
 	def recordFile(self, fileName):
 		"""Updates a record in persistence that the file has been Ingested."""
 		pass
 
-	def determineFileType(self, fileName):
+	def determineDataType(self, fileName):
 		"""Determines the type of file, based on extension. Used to populate the Ingest Job."""
-		pass
+		name, extension = os.path.splitext(fileName)
+		if extension.lower() in ('tif', 'tiff', 'geotiff', 'tfw'):
+			return 'raster'
+		if extension.lower() in ('laz', 'las', 'bpf'):
+			return 'pointcloud'
+		return None
+
+	def destroy(self):
+		"""Cleans up persistence file resources upon termination."""
+		self.persistenceFile.close()
 
 def main():
 	"""Instantiates a FileWatcher based on environment variables, or command line args."""
@@ -75,6 +130,9 @@ def main():
 	# Begin listening
 	fileWatcher = FileWatcher(bucket, accessKey, privateKey, pzApiKey)
 	fileWatcher.listen()
+
+	# When application terminates, ensure FileWatcher persistence buffer is flushed 
+	atexit.register(fileWatcher.destroy)
 
 if __name__ == "__main__":
 	main()
